ca6564cdc1bb554ae3fccef817cb69399d9be2bb
John Varghese <lookitup4me@gmail.com>
2024-02-22 16:17:06 -0800

    clean up openai uitls

--- openai_utils.py
+++ openai_utils.py
@@ -13,21 +13,6 @@ def generate_embedding(text_chunk):
     return embedding_vector
 
 
-from openai import OpenAI
-
-text = """Older adults form a fast-increasing proportion of the world population. However, gains in increasing quantity of life have not been accompanied by similar gains in quality of life. Older people frequently experience frailty, memory problems, and chronic diseases including cardiovascular disease (CVD) and neurodegenerative diseases. Recent trials have demonstrated the efficacy of anti-hypertensive therapy in older populations but failed to show benefits for aspirin. Statins clearly reduce CVD events in middle-aged populations. There seems to be evidence that the effect is similar in primary prevention older populations based on meta-analyses mainly from sub-groups in large trials, but this becomes less clear with increasing age. However, given differences in drug metabolism and possibly efficacy, competing co-morbidities, their effects on mortality, disability, and dementia in this age group remain to be determined."""
-system = [{"role": "system", "content": "You are Summary AI."}]
-user = [{"role": "user", "content": f"Summarize this briefly:\n\n{text}"}]
-chat_history = []  # past user and assistant turns, for AI memory
-
-chat_completion = client.chat.completions.create(
-  messages = system + chat_history + user,
-  model="gpt-3.5-turbo",
-  max_tokens=500, top_p=0.9,
-  )
-print(chat_completion.choices[0].message.content)
-
-
 def generate_summary(text_chunk):
     system = [{"role": "system", "content": "You are Summary AI."}]
     user = [{"role": "user", "content": f"Summarize this briefly:\n\n{text_chunk}"}]

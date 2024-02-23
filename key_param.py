6a514c70b3d3470193e96b7a8d2743b2cb0bc1b5
John Varghese <lookitup4me@gmail.com>
2024-02-22 16:32:34 -0800

    add keys tempoarily

---
+++ key_param.py
@@ -0,0 +1,2 @@
+openai_api_key="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"
+MONGO_URI="mongodb+srv://chunking_user:Hiq22idKv94I0YQN@cluster0.jqn2q4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
